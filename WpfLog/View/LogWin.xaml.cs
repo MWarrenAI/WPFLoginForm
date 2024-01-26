f39e5d36a68ec6b9f56ad2adb5785b857b3f402b
MStaffbane <42208427+MStaffbane@users.noreply.github.com>
2024-01-26 07:13:30 +0000

    Project Upload

---
+++ WpfLog/View/LogWin.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLog.View
+{
+    /// <summary>
+    /// Interaction logic for LogWin.xaml
+    /// </summary>
+    public partial class LogWin : Window
+    {
+        public LogWin()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e) 
+        { 
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
